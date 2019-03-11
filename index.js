7ef9df7a6df9fb9cbe965fe61f5807b710393726
spielhoelle <spiel.hoelle@live.de>
2019-03-11 15:02:06 +0100

    use async await

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const promisifiedRequest = util.promisify(request);
 
 const START_URL = "http://www.taz.de";
 
-promisifiedRequest(START_URL).then(response => {
+(async() => {
+  const response = await promisifiedRequest(START_URL)
   console.time("TimeConsumed");
   console.log('response', response.body.slice(0, 200));
-  console.timeEnd("TimeConsumed");
-});
+  console.timeEnd("TimeConsumed");  
+})()
+
