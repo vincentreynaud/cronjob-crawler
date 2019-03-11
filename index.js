3f3e47cc118e0b52688bb99991943a99445bd5f8
spielhoelle <spiel.hoelle@live.de>
2019-03-11 14:50:26 +0100

    refactor to promisified request version, avoid var

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var request = require("request");
-var cheerio = require('cheerio');
+const request = require("request");
+const cheerio = require('cheerio');
 const util = require('util');
 const promisifiedRequest = util.promisify(request);
 
-var START_URL = "http://www.taz.de";
+const START_URL = "http://www.taz.de";
 
 promisifiedRequest(START_URL).then(response => {
   console.time("TimeConsumed");
-  console.log('body', response.body.slice(0, 200));
+  console.log('response', response.body.slice(0, 200));
   console.timeEnd("TimeConsumed");
 });
