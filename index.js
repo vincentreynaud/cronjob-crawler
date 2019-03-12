689dbf808df88c812fe4174d899f21f40b92bb00
spielhoelle <spiel.hoelle@live.de>
2019-03-12 10:15:39 +0100

    cronjob crawler

--- index.js
+++ index.js
@@ -3,16 +3,42 @@ const cheerio = require('cheerio');
 const util = require('util');
 const promisifiedRequest = util.promisify(request);
 
-const START_URL = "http://www.taz.de";
-
 //IIFE
-(async() => {
-  const response = await promisifiedRequest(START_URL)
+(async () => {
+  const list = [
+    {link: 'https://www.tbd.community/en/jobs?r=206#search', selector: ".job.card .title"},
+    {link: 'http://berlinstartupjobs.com/engineering', selector: ".product-listing-h2 a"},
+    // TODO surf the web and find job offering-pages and investigate their title-selector
+    // check if the pages respond with html or are a SPA. If they are just javascript, dont use them - its way harder to parse their content.
+    // just curl the domain and check if you can see the Job in the returned html
+    // if the page returns good ol' html, investigate the selector for the title element
+    // ...
+  ]
+  const terms = [
+    'software developer',
+    'software entwickler',
+    'developer'
+    // TODO add all possible job titles you wanna look for
+  ]
+  //TODO optimze this loop to use multiple parallel requests instead of the current seriell way
   console.time("TimeConsumed");
-  const $ = cheerio.load(response.body)
-  //fetch all titles from articles and concatinate them to a string
-  console.log($('a.article h3').text())
+  for (let site of list) {
+    const response = await promisifiedRequest(site.link)
 
-  //TODO find selector and `each` over it https://cheerio.js.org/#each-functionindex-element-
-  console.timeEnd("TimeConsumed");  
+    const jobs = [];
+    const $ = cheerio.load(response.body)
+    $(site.selector).each(function (i, elem) {
+      jobs[i] = $(this).text();
+    });
+    //TODO loop over terms and find your jobtitles by term
+    for (let term of terms){
+
+    }
+    console.log('Found jobs: ', jobs);
+    //TODO save found jobs to MLAB mongo
+    //TODO send a mail with new jobs and already found jobs to your self
+  }
+  console.timeEnd("TimeConsumed");
 })()
+
+//TODO use https://www.npmjs.com/package/node-cron to run the crawler in a regular interval
